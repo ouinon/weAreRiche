35ba33fd4b0884cdf269f08ed49c3c2f4980ffaa
Imowt <valentin76133@gmail.com>
2017-08-22 18:21:48 +0200

    rename login.xml

--- app/src/main/java/com/example/val_7/myapplication/MainLogin.java
+++ app/src/main/java/com/example/val_7/myapplication/MainLogin.java
@@ -3,7 +3,6 @@ package com.example.val_7.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.DigitsKeyListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,7 +21,7 @@ public class MainLogin extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout);
+        setContentView(R.layout.activity_login);
 
         signin = ((Button) findViewById(R.id.signin));
         signup = ((Button) findViewById(R.id.signup));
